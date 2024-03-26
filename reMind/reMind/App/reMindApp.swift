6a76b3f1be39ada944552946c7e0291e2d557d82
˜Flavio <flaviomdev@gmail.com>
2024-03-25 23:16:21 -0300

    setup reMind

---
+++ reMind/reMind/App/reMindApp.swift
@@ -0,0 +1,17 @@
+//
+//  reMindApp.swift
+//  reMind
+//
+//  Created by Flávio Mattos on 25/03/24.
+//
+
+import SwiftUI
+
+@main
+struct reMindApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ reMind/reMind/Features/Boxes/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  reMind
+//
+//  Created by Flávio Mattos on 25/03/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
